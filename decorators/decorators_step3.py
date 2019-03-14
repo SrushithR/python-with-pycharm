f02b78aa3e954d1a61fc3068aabcd14c6352b723
srushith <srushith.r@gmail.com>
2019-03-15 00:42:22 +0530

    created timer function for time functionality abstractions

--- decorators/decorators_step3.py
+++ decorators/decorators_step3.py
@@ -17,12 +17,19 @@ def get_profit(sales_amount, movie_type) -> float:
     return profit
 
 
-def timer(movie_type, sales_amount):
+def timer(func, movie_type, sales_amount):
+    """
+        Function to calculate the time taken by a function
+    :param func: name of the function to be called
+    :param movie_type: type of the movie - blockbuster, hit, etc
+    :param sales_amount: total sales amount for the particular movie
+    :return: return value of the function called
+    """
     before = time()
-
+    return_value = func(movie_type, sales_amount)
     after = time()
     print("time elapsed: ", after - before)
-    return
+    return return_value
 
 
 def get_taxes(sales_amount, movie_type) -> float:
@@ -32,8 +39,6 @@ def get_taxes(sales_amount, movie_type) -> float:
     :param movie_type: type of the movie - blockbuster, hit, etc
     :return: tax amount
     """
-    before = time()
-
     if movie_type == 1:
         taxes = sales_amount * 0.05
     elif movie_type == 2:
@@ -41,16 +46,13 @@ def get_taxes(sales_amount, movie_type) -> float:
     else:
         taxes = sales_amount * 0.03
 
-    after = time()
-    print("time elapsed: ", after - before)
-
     return taxes
 
 
-print("get_profit(10000, 1)", get_profit(10000, 1))
-print("get_profit(10000, 2)", get_profit(10000, 2))
-print("get_profit(10000, 3)", get_profit(10000, 3))
+print("get_profit(10000, 1)", timer(get_profit, 10000, 1))
+print("get_profit(10000, 2)", timer(get_profit, 10000, 2))
+print("get_profit(10000, 3)", timer(get_profit, 10000, 3))
 
-print("get_taxes(10000, 1)", get_taxes(10000, 1))
-print("get_taxes(10000, 2)", get_taxes(10000, 2))
-print("get_taxes(10000, 3)", get_taxes(10000, 3))
+print("get_taxes(10000, 1)", timer(get_taxes, 10000, 1))
+print("get_taxes(10000, 2)", timer(get_taxes, 10000, 2))
+print("get_taxes(10000, 3)", timer(get_taxes, 10000, 3))
